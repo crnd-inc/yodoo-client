131009deb235eb24c8963c0c9e85dd844e091a6a
Ігор Безобюк <igor.bezobiuk@crnd.pro>
2018-06-22 18:01:22 +0300

    Added controller to module.

---
+++ controllers/odoo_infrastructure_auth.py
@@ -0,0 +1,47 @@
+import json
+from uuid import uuid4 as uuid
+from datetime import datetime, timedelta
+
+from odoo import http, registry, _
+from odoo.tools import config
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
+from odoo.service.db import exp_db_exist
+
+
+def _prepare_temporary_auth_data(ttl):
+    ttl = ttl or 3600
+    return {'token_user': str(uuid()),
+            'token_password': str(uuid()),
+            'expire': ((datetime.now() + timedelta(seconds=int(ttl))).
+                       strftime(DATETIME_FORMAT))}
+
+
+class OdooInfrastructureAuth(http.Controller):
+
+    @http.route(
+        '/odoo/infrastructure/auth',
+        type='http',
+        auth='none',
+        db='none',
+        metods=['POST'],
+        csrf=False
+    )
+    def create_temporary_login_data(self, **params):
+        data = {}
+        if exp_db_exist(params['db']):
+            if (config.get('odoo_infrastructure_token') ==
+                    params['odoo_infrastructure_token']):
+                data = _prepare_temporary_auth_data(params['ttl'])
+                with registry(params['db']).cursor() as cr:
+                    cr.execute(
+                        'INSERT INTO odoo_infrastructure_client_auth '
+                        '(token_user, token_password, expire) VALUES '
+                        '(%(token_user)s, %(token_password)s, %(expire)s);',
+                        data
+                    )
+            else:
+                data['error'] = _('Token does not match.')
+        else:
+            data['error'] = _('DB with this name is not found.')
+
+        return json.dumps(data)
