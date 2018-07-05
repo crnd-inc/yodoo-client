e3131481327214c9176cc904b9467b55a41b6762
Ігор Безобюк <igor.bezobiuk@crnd.pro>
2018-07-05 17:48:44 +0300

    Added half common tests.

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import hashlib
 import string
 import unittest
 
+from datetime import datetime, timedelta
 from random import shuffle
 from os import environ
 from urllib.parse import urlunsplit
@@ -38,70 +39,117 @@ class TestOdooInfrastructureAuth(unittest.TestCase):
              '',
              '')
         )
+        cls._data = {
+            'odoo_infrastructure_token': cls._hash_token,
+            'ttl': 300,
+            'db': cls._client.dbname
+        }
 
     @classmethod
     def tearDownClass(cls):
         cls._odoo_instance.services.db.drop_db('admin', cls._db_name)
 
+
+class TestOdooInfrastructureAuthAuth(TestOdooInfrastructureAuth):
     def setUp(self):
-        self.data = {
-            'odoo_infrastructure_token': self._hash_token,
-            'ttl': 300,
-            'db': self._client.dbname
+        self.correct_response_keys = {
+            'token_password',
+            'token_user',
+            'expire',
+            'temp_url',
+            'token_temp'
         }
+        self.incorrect_response_keys = {'error'}
 
     def test_01_controller_odoo_infrastructure_auth(self):
-        print('1')
 
         # test correct request
-
-        response = requests.post(self._url, data=self.data)
+        response = requests.post(self._url, data=self._data)
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            set(response.json().keys()),
+            self.correct_response_keys
+        )
+
 
     def test_02_controller_odoo_infrastructure_auth(self):
-        print('2')
 
         # test incorrect request with bad odoo_infrastructure_token
-
-        data = self.data.copy()
+        data = self._data.copy()
         data.update({
             'odoo_infrastructure_token': 'abracadabra'
         })
-        print(data)
 
         response = requests.post(self._url, data=data)
-        print(response.status_code)
-        print(response.text)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            set(response.json().keys()),
+            self.incorrect_response_keys
+        )
 
-        data = self.data.copy()
+    def test_03_controller_odoo_infrastructure_auth(self):
+
+        # test incorrect request with bad db_name
+        data = self._data.copy()
         data.update({
             'db': 'abracadabra'
         })
 
-
         response = requests.post(self._url, data=data)
         self.assertEqual(response.status_code, 404)
-#
-        #data.update({
-        #    'odoo_infrastructure_token': 'abracadabra'
-        #})
-#
-        #response = requests.post(self._url, data=data)
-        #self.assertEqual(response.status_code, 404)
-#
-        #data.update({
-        #    'odoo_infrastructure_token': self._hash_token,
-        #    'db': 'abracadabra'
-        #})
-#
-        #response = requests.post(self._url, data=data)
-        #self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            set(response.json().keys()),
+            self.incorrect_response_keys
+        )
+
+    def test_04_controller_odoo_infrastructure_auth(self):
+
+        # test scheduler remove row after expire
+        response = requests.post(self._url, data=self._data)
+        OdooInfrastructureClientAuth = self._client['odoo.infrastructure.client.auth']
+
+        # check if record exist
+        temp_rows = OdooInfrastructureClientAuth.search_records(
+            [('token_temp', '=', response.json()['token_temp'])]
+        )
+        self.assertEqual(len(temp_rows), 1)
+
+        # run scheduler
+        OdooInfrastructureClientAuth.scheduler_cleanup_expired_entries()
+
+        # check if record exist still
+        temp_rows = OdooInfrastructureClientAuth.search_records(
+            [('token_temp', '=', response.json()['token_temp'])]
+        )
+        self.assertEqual(len(temp_rows), 1)
+
+        # change expire
+        temp_rows[0].write({'expire':
+                                (datetime.strptime(
+                                    temp_rows[0]['expire'], "%Y-%m-%d %H:%M:%S") -
+                                 timedelta(hours=1)
+                                 ).strftime("%Y-%m-%d %H:%M:%S")})
+
+        # run scheduler
+        OdooInfrastructureClientAuth.scheduler_cleanup_expired_entries()
+
+        # check if record does not exist
+        temp_rows = OdooInfrastructureClientAuth.search_records(
+            [('token_temp', '=', response.json()['token_temp'])]
+        )
+        self.assertEqual(len(temp_rows), 0)
+
+
+class TestOdooInfrastructureAuthSaasAuth(TestOdooInfrastructureAuth):
+    def setUp(self):
+        pass
 
 def suite():
     _suite = unittest.TestSuite()
-    _suite.addTest(TestOdooInfrastructureAuth('test_01_controller_odoo_infrastructure_auth'))
-    _suite.addTest(TestOdooInfrastructureAuth('test_02_controller_odoo_infrastructure_auth'))
+    _suite.addTest(TestOdooInfrastructureAuthAuth('test_01_controller_odoo_infrastructure_auth'))
+    _suite.addTest(TestOdooInfrastructureAuthAuth('test_02_controller_odoo_infrastructure_auth'))
+    _suite.addTest(TestOdooInfrastructureAuthAuth('test_03_controller_odoo_infrastructure_auth'))
+    _suite.addTest(TestOdooInfrastructureAuthAuth('test_04_controller_odoo_infrastructure_auth'))
     return _suite
 
 
