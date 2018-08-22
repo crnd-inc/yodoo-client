a47931f46eec8ede005fd26e25cbfc2444cfd7ee
Ігор Безобюк <igor.bezobiuk@crnd.pro>
2018-08-22 19:19:12 +0300

    Changed version of module.

--- odoo_infrastructure_client/__manifest__.py
+++ odoo_infrastructure_client/__manifest__.py
@@ -8,7 +8,7 @@
     'website': "https://crnd.pro",
     'license': 'Other proprietary',
 
-    'version': '11.0.0.4.0',
+    'version': '11.0.0.4.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
