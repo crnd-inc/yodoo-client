dca0ea25f3feee44ad261b3686933ee9e53465d4
Ostap Yakovenko <ceo@crnd.pro>
2020-10-02 15:40:49 +0300

    Init migrate to 14.0

--- yodoo_easy_backup/__manifest__.py
+++ yodoo_easy_backup/__manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://crnd.pro",
     'license': 'Other proprietary',
 
-    'version': '13.0.0.0.1',
+    'version': '14.0.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': [
